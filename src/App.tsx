0edf92668fada83172706f77317691d3e9cc0de3
DLew <96327749+DLew2024@users.noreply.github.com>
2025-02-16 13:15:27 -0600

    Using Navigation for the website

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import LandingPage from './components/LandingPage';
+
 function App() {
-  return <>App</>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
